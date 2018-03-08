11846f6113243a67d7d8982d1a16f46ef39fd456
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-03-08 14:23:41 -0800

    Working on a test

---
+++ tests.pl
@@ -0,0 +1,12 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(prolog/openapi).
+
+:- openapi(petstore, []).
+
+server(Port) :-
+    http_server(openapi_dispatch,
+                [ port(Port)
+                ]).
+
+listPets(_Limit, Pets) :-
+    Pets = [p1, p2, p3].
