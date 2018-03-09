4ee2d0854ca55c0ad7e47f199336a8aa09a47a25
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-03-08 21:52:30 -0800

    Extend tests

--- client.pl
+++ client.pl
@@ -41,3 +41,11 @@ showPetById(Id, Pet) :-
         json_read_dict(In, Pet, []),
         close(In)),
     writeln(Status).
+
+fill_db :-
+    read_file_to_terms('pets.db', Terms, []),
+    foldl(add_pet, Terms, 1, _).
+
+add_pet(pet(Name, _Sort, _Gender), N, N2) :-
+    N2 is N + 1,
+    createPets([_{id:N, name:Name}]).
