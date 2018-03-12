10c07ae12c23dc4224c3649129680e6d1de4b47c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-03-12 11:07:18 -0700

    Fill entire DB

--- pet_client.pl
+++ pet_client.pl
@@ -8,3 +8,12 @@
 %!  createPets(+Pets)
 
 %!  showPetById(+Id, -Pet)
+
+
+fill_db :-
+    read_file_to_terms('pets.db', Terms, []),
+    foldl(add_pet, Terms, 1, _).
+
+add_pet(pet(Name, _Sort, _Gender), N, N2) :-
+    N2 is N + 1,
+    createPets([_{id:N, name:Name}], _).
