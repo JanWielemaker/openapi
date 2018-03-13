ba71ff70e6786179dec95f985557ea3485e8dc20
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-03-13 15:09:43 -0700

    Packaging and wrapper

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(openapi).
+version('0.1').
+title('OpenAPI (Swagger) interface').
+keywords(['OpenAPI', 'Swagger', 'REST', 'HTTP', service]).
+author( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
+home( 'https://github.com/JanWielemaker/openapi.git' ).
+download( 'https://github.com/JanWielemaker/openapi/archive/V*.zip' ).
