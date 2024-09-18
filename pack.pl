52bb2e06770402ff9a894b568980a7135db2e8bb
Jan Wielemaker <jan@swi-prolog.org>
2024-09-18 17:32:37 +0200

    Preparing 0.7.0

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(openapi).
-version('0.2.1').
+version('0.7.0').
 title('OpenAPI (Swagger) interface').
 keywords(['OpenAPI', 'Swagger', 'REST', 'HTTP', service]).
 author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
 packager( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
 maintainer( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
 home( 'https://github.com/JanWielemaker/openapi.git' ).
-download( 'https://github.com/JanWielemaker/openapi/archive/V*.zip' ).
+download( 'https://github.com/JanWielemaker/openapi.git' ).
